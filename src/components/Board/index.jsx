768d8eca1f6bd8e15f91d48f3a0645b96f87b0c0
Daniel Josias <daniieljosiaslima@gmail.com>
2023-06-20 17:06:14 -0300

    feat: board

---
+++ src/components/Board/index.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { loadList } from "../../services/api"
+import BoardContext from './context'
+import produce from 'immer'
+const data = loadList()
+
+export const Dashboard = () => {
+    const [lists, setLists] = useState(data)
+    
+    const move = (fromList, toList, from, to) =>{
+        setLists(produce(lists, draft => {
+        const dragged = draft[fromList].cards[from];
+
+        draft[fromList].cards.splice(from, 1);
+        draft[toList].cards.splice(to, 0, dragged);
+        }))
+    }
+
+    return(
+        <BoardContext.Provider value={{lists, move}}>
+            <Header/>
+            {/* {lists.map((list, index) => <List key={list.id} index={index} data={list} />)} */}
+        </BoardContext.Provider>
+    )
+}
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+export const loadList = () =>{
+    return [
+        {   id: 1,
+            title: 'To do',
+            creatable: true,
+            cards: [
+                {
+                    id: 1,
+                    content: 'criar o header'
+                },
+                {
+                    id: 2,
+                    content: 'criar o dashboard'
+                },
+                {
+                    id: 3,
+                    content: 'criar o footer'
+                },
+            ]
+        }
+    ]
+}
