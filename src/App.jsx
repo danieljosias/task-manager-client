1af1878aeda2e4cf3aa66f7aa039fcee22972c39
Daniel Josias <daniieljosiaslima@gmail.com>
2023-06-30 10:20:40 -0300

    feat: welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { DndProvider } from "react-dnd"
 import HTML5Backend from 'react-dnd-html5-backend'
-import { Router } from './routes'
+import { Router } from './routes/routes'
 
 export const App = () => {
   return (
