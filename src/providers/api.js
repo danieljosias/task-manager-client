4a29b6a833d76320abb7e11d433ffe29b3110c44
Daniel Josias <daniieljosiaslima@gmail.com>
2023-07-01 18:57:18 -0300

    feat: providers

---
+++ src/providers/api.js
@@ -0,0 +1,85 @@
+import { api } from '../services/index'
+
+export const ApiProvider = ({children}) => {
+    //let token = localStorage.getItem('token')
+
+    async function createsUsers(data){
+        try {
+            const res = await api.post('/clients', data)
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function login(data){
+        try {
+            const res = await api.post('/login', data)
+            return res
+        } catch (error) {
+            return error
+        }   
+    }   
+
+    async function createsTasks(data, userId){
+        try {
+            const res = await api.post('/tasks', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    }  
+
+    async function listTasks(data, userId){
+        try {
+            const res = await api.get('/tasks', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    } 
+
+    async function updateTasks(data, userId){
+        try {
+            const res = await api.patch('/tasks', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    } 
+
+    async function deleteTasks(data, userId){
+        try {
+            const res = await api.delete('/tasks', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function createsLists(data, userId){
+        try {
+            const res = await api.post('/lists', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    async function listLists(data, userId){
+        try {
+            const res = await api.get('/lists', {data,userId,token})
+            return res
+        } catch (error) {
+            return error
+        }
+    }
+
+    return(
+        <ApiContext.Provider
+            value={{createsUsers,login,createsTasks,createsLists,updateTasks,deleteTasks,listTasks,listLists}}
+        >
+            {children}
+        </ApiContext.Provider>
+    )
+}
